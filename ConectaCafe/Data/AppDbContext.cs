962e474115311ddb8954b2beabaa93e56a1ed873
AnaAbrussi <analuizaabrussi@gmail.com>
2024-03-28 14:24:10 -0300

    Alterações

--- ConectaCafe/Data/AppDbContext.cs
+++ ConectaCafe/Data/AppDbContext.cs
@@ -18,13 +18,25 @@ namespace ConectaCafe.Data;
          public DbSet<Tag> Tags { get; set; }
 
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder Builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(Builder);
 
         // FluentAPI
         #region muitos para muitos do BlogTag
-        
+        Builder.Entity<BlogTag>().HasKey(
+            bt => new { bt.BlogId, bt.TagId }
+        );
+        Builder.Entity<BlogTag>()
+            .HasOne(bt => bt.Blog)
+            .WithMany(b => b.BlogTags)
+            .HasForeignKey(bt => bt.BlogId);
+
+        Builder.Entity<BlogTag>()
+            .HasOne(bt => bt.Tag)
+            .WithMany(t => t.BlogTags)
+            .HasForeignKey(bt => bt.TagId);
+
         #endregion
 
     }
